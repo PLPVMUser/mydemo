302fc6e5ec157b7b17abef8ea39f1813aa4c08ef
www-data <www-data@ubuntu1.(none)>
2012-07-28 01:42:35 -0400

    changes

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+require "bundler/capistrano"
+require 'rvm/capistrano'
+set :rvm_ruby_string, 'ruby-1.9.3-p194@working'
+set :rvm_type, :system 
+
+set :use_sudo, false
+
+set :application, "mydemo"
+set :deploy_to, "/var/www/#{application}"
+
+set :repository, "git@github.com:eraserx99/mydemo.git"
+set :branch, "master"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "localhost"                          # Your HTTP server, Apache/etc
+role :app, "localhost"                          # This may be the same as your `Web` server
+role :db,  "localhost", :primary => true     # This is where Rails migrations will run
+
+default_run_options[:pty] = true
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
